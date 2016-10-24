7b5216d27cd8f32a3cd568a2900de55d6a39d299
Aditya Agarwal <adityag2511@gmail.com>
2016-10-24 22:27:45 +0530

    Java Servlet provinding mailing service

---
+++ src/src/com/javatpoint/ConProvider.java
@@ -0,0 +1,15 @@
+package com.javatpoint;
+import java.sql.*;
+public class ConProvider {
+
+	public static Connection getConnection(){
+	Connection con=null;
+	try{
+//		Class.forName("oracle.jdbc.driver.OracleDriver");
+//		con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","oracle");
+                Class.forName("com.mysql.jdbc.Driver");
+                con=DriverManager.getConnection("jdbc:mysql://localhost:3306/employee_mailer","root","Shobhnaj173"); 
+	}catch(Exception e){System.out.println(e);}
+	return con;
+    }
+}
